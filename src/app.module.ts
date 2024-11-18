9a39b71ce9b8c50a299a30a547ec52282f698cee
nhan6104 <thanhan.nguyen6104@gmail.com>
2024-11-18 22:55:35 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { PrinterModule } from './modules/printer/printer.module';
 import { UserModule } from './modules/user/user.module';
+import { ReportModule } from './modules/report/report.module';
+import { SystemConfigurationModule } from './modules/system/system.module';
 
 @Module({
-  imports: [DatabaseModule, PrinterModule, UserModule],
+  imports: [DatabaseModule, PrinterModule, UserModule, ReportModule, SystemConfigurationModule],
   controllers: [AppController],
   providers: [AppService],
 })
