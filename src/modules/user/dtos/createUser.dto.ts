a86f1c90d2035298f03616360af88f8cf10e1a60
Nguyen Khanh Binhe <nguyenkhanhbinh2004@gmail.com>
2024-11-19 01:13:39 +0700

    Chuẩn bị trước khi code.

---
+++ src/modules/user/dtos/createUser.dto.ts
@@ -0,0 +1,9 @@
+export class CreateUserDto {
+    password: string
+    username: string
+    name: string
+    email: string
+    phoneNumber: string
+    role: string
+  
+  }
---
+++ src/modules/user/entities/user.entity.ts
@@ -0,0 +1,48 @@
+import { UUID } from 'crypto';
+import { UUIDV4 } from 'sequelize';
+import {
+    Table,
+    Column,
+    Model,
+    DataType,
+    PrimaryKey,
+    Default,
+    AllowNull,
+    HasMany,
+    Unique,
+    ForeignKey,
+    BelongsTo,
+    AutoIncrement,
+} from 'sequelize-typescript';
+
+@Table
+export class User extends Model<User> {
+    @PrimaryKey
+    @AutoIncrement
+    @Column(DataType.INTEGER)
+    sso_id: number;
+
+    @AllowNull(false)
+    @Column(DataType.STRING)
+    password: String;
+
+    @AllowNull(false)
+    @Column(DataType.STRING)
+    name: string;
+
+    @AllowNull(false)
+    @Column(DataType.STRING)
+    username: String;
+
+    @AllowNull(false)
+    @Column(DataType.STRING)
+    email: String;
+
+    @AllowNull(false)
+    @Column(DataType.STRING)
+    phone: String;
+
+    @AllowNull(false)
+    @Column(DataType.STRING)
+    role: string;
+}
